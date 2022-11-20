f3daeb2af88fbacbb10a0df9e884b69e37d09d36
Alec Graham <maaaachoalpha@gmail.com>
2022-11-20 11:58:40 -0800

    this is a test to merge

--- Social_Media_BackEnd/src/main/java/dev/jsonmusk/util/ConnectionFactory.java
+++ Social_Media_BackEnd/src/main/java/dev/jsonmusk/util/ConnectionFactory.java
@@ -7,16 +7,18 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public static Connection getConnection() {
         // for Alec
+
+        /// this is a test
         //////////////////////////////////////////////////////////////////////////////////////
-        //String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=p5";
-        //String username = "postgres";
-        //String password = "password";
+        String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=p5";
+        String username = "postgres";
+        String password = "password";
 
         // for not Alec
         //////////////////////////////////////////////
-        String url = System.getenv("POSTGRES_SQL_DB");
-        String username = System.getenv("DB_USERNAME");
-        String password = System.getenv("PASSWORD");
+        //String url = System.getenv("POSTGRES_SQL_DB");
+        //String username = System.getenv("DB_USERNAME");
+        //String password = System.getenv("PASSWORD");
         try {
             return DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
