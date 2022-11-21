d07ca367648cf2d370649ab5e5689ecba2e85a50
Alec Graham <maaaachoalpha@gmail.com>
2022-11-21 14:11:23 -0800

    added search functionality

--- Social_Media_BackEnd/src/main/java/dev/jsonmusk/util/ConnectionFactory.java
+++ Social_Media_BackEnd/src/main/java/dev/jsonmusk/util/ConnectionFactory.java
@@ -8,17 +8,17 @@ public class ConnectionFactory {
     public static Connection getConnection() {
         // for Alec
 
-        /// this is a test
-        //////////////////////////////////////////////////////////////////////////////////////
-   //     String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=p5";
-    //    String username = "postgres";
-    //    String password = "password";
+        // this is a test
+        ////////////////////////////////////////////////////////////////////////////////////
+        String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=p5";
+        String username = "postgres";
+        String password = "password";
 
         // for not Alec
-        //////////////////////////////////////////////
-        String url = System.getenv("POSTGRES_SQL_DB");
-        String username = System.getenv("DB_USERNAME");
-        String password = System.getenv("PASSWORD");
+//        //////////////////////////////////////////////
+//        String url = System.getenv("POSTGRES_SQL_DB");
+//        String username = System.getenv("DB_USERNAME");
+//        String password = System.getenv("PASSWORD");
         try {
             return DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
